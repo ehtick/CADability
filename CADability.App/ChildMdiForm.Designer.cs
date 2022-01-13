f5dca22f82b31c9a3f9b688af69acb686b620d11
Michel Metzler <mmetzler@dip-concept.de>
2022-01-13 13:17:32 +0100

    Update ChildMdiForm

--- CADability.App/ChildMdiForm.Designer.cs
+++ CADability.App/ChildMdiForm.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.cadControl1 = new CADability.Forms.CadControl();
             this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // cadControl1
@@ -46,25 +50,57 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(296, 256);
+            this.button1.Location = new System.Drawing.Point(12, 16);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(139, 23);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Force Handle Recreation";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 350);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 100);
+            this.panel1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Split error";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(157, 16);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "CompoundShape";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // ChildMdiForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.cadControl1);
             this.KeyPreview = true;
             this.Name = "ChildMdiForm";
             this.Text = "ChildMdiForm";
             this.Load += new System.EventHandler(this.ChildMdiForm_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -73,5 +109,8 @@
 
         private Forms.CadControl cadControl1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
--- CADability.App/ChildMdiForm.cs
+++ CADability.App/ChildMdiForm.cs
@@ -1,4 +1,6 @@
-﻿using CADability.Shapes;
+﻿using CADability.Curve2D;
+using CADability.GeoObject;
+using CADability.Shapes;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -21,10 +23,10 @@ namespace CADability.App
 
         private void ChildMdiForm_Load(object sender, EventArgs e)
         {
-            
-            
-            Project newProj = Project.CreateSimpleProject();            
-            cadControl1.CadFrame.Project = newProj; 
+
+
+            Project newProj = Project.CreateSimpleProject();
+            cadControl1.CadFrame.Project = newProj;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -32,6 +34,80 @@ namespace CADability.App
             if (this.RightToLeft == System.Windows.Forms.RightToLeft.Yes)
                 this.RightToLeft = System.Windows.Forms.RightToLeft.No;
             else this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var m = cadControl1.CadFrame.Project.GetActiveModel();
+
+            List<ICurve> curves = new List<ICurve>(m.AllObjects.Count);
+            foreach (IGeoObject geo in m.AllObjects)
+                if (geo is ICurve geoCurve)
+                    curves.Add(geoCurve);
+
+            if (!Curves.GetCommonPlane(curves, out Plane pln))
+                return;
+
+            List<ICurve2D> curves2D = new List<ICurve2D>(curves.Count);
+
+            foreach (ICurve curve in curves)
+                curves2D.Add(curve.GetProjectedCurve(pln));
+
+            var cs = CompoundShape.CreateFromList(curves2D.ToArray(), 0.01, true, out GeoObjectList deads);
+
+            Path2D ssOutlines = cs.SimpleShapes[0].Outline.AsPath();
+            for (int i = 1; i < cs.SimpleShapes.Length; i++)
+                ssOutlines = SimpleShape.ConnectPaths(ssOutlines, cs.SimpleShapes[i].Outline.AsPath());
+
+            //Create Convex Hull
+            var hull = ssOutlines.MakeBorder().ConvexHull();
+
+            //Convert Border of Convex Hull to GeoObjects
+            var segements = hull.GetClonedSegments();
+            GeoObjectList geos = new GeoObjectList(segements.Length);
+            foreach (ICurve2D i in segements)
+            {
+                i.Reverse();
+                IGeoObject geoObject = i.MakeGeoObject(pln);
+                geos.Add(geoObject);
+            }
+            // Add Convex Hull Border to Model
+            m.Add(geos);
+
+            //Create smalles surrounding rect
+            var ssr = ssOutlines.MakeBorder().GetSmallestEnclosingRectangle();
+
+            //Calc lower left start point
+            var lowerleft = pln.ToGlobal(ssr.FixPoint);
+
+
+            GeoObject.Polyline line = GeoObject.Polyline.Construct();
+            line.SetRectangle(lowerleft, new GeoVector(ssr.Width, 0, 0), new GeoVector(0, ssr.Height, 0));
+            line.ParallelogramMainDirection = new GeoVector(ssr.BasisDir);            
+            
+            //m.Add(line);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var m = cadControl1.CadFrame.Project.GetActiveModel();
+
+            List<ICurve> curves = new List<ICurve>(m.AllObjects.Count);
+            foreach (IGeoObject geo in m.AllObjects)
+                if (geo is ICurve geoCurve)
+                    curves.Add(geoCurve);
+
+            if (!Curves.GetCommonPlane(curves, out Plane pln))
+                return;
+
+            List<ICurve2D> curves2D = new List<ICurve2D>(curves.Count);
+
+            foreach (ICurve curve in curves)
+                curves2D.Add(curve.GetProjectedCurve(pln));
+
+            var cs = CompoundShape.CreateFromList(curves2D.ToArray(), 0.01, true, out GeoObjectList deads);
+
+
         }
     }
 }
